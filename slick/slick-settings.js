37c2116e2eec8769a636b66462c0c13c7b2dfe51
rovied <rovied@yandex.ru>
2016-08-19 12:10:19 +0300

    Release snowboard

---
+++ slick/slick-settings.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	$('.center').slick({
+		centerMode: true,
+		slidesToShow: 3,
+		variableWidth: true,
+		prevArrow: '<button type="button" class="slider__prev-arrow"></button>',
+		nextArrow: '<button type="button" class="slider__next-arrow"></button>'
+	});
+
+});
